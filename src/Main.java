f5d4bd26fa1b198f703eb72a30a395ad2d9d7eb2
JD <79108823+JMDProgrammer@users.noreply.github.com>
2022-05-31 13:05:00 -0400

    changes made in the main
    changes made in the main for design by contract. added a bunch of if and else if statements

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 public class Main {
+<<<<<<< Updated upstream
     public static void main(String[] args) {
 
         //testing intellij ide
@@ -46,5 +47,49 @@ public class Main {
 
     public static int testClass(int fun){
     return fun;
+=======
+
+    public static void main(String[] args) { //caller
+
+        //used for the user to determine size
+        int max =4;
+        MyStackDC2 myStackDC2 = new MyStackDC2();
+
+
+
+
+        //for the user to check if the stack is not full
+        if(!myStackDC2.isFull(max)) {
+            //stack will be able to push
+            myStackDC2.push(1);
+            System.out.println("the stack is full");
+            //for the user to check if the stack is empty
+
+            //for the user to check if the stack is not empty
+        }else if(!myStackDC2.isEmpty()){
+            //stack will be able to pop
+            System.out.println(myStackDC2.peek());
+            myStackDC2.pop();
+        }else if(myStackDC2.isFull(max)){
+            //stack will not be able to push
+            System.out.println("the stack is empty");
+        //for the user to check if the stack is empty
+        } else if(myStackDC2.isEmpty()) {
+        //stack will not be able to pop
+        System.out.println("The stack is empty");
+    }
+
+    /*
+        MyStackDD stackDD = new MyStackDD();
+        stackDD.pop();
+        stackDD.push(1);
+        System.out.println();
+        stackDD.push(2);
+        stackDD.push(3);
+        stackDD.push(4);
+        stackDD.push(5);
+      */
+
+>>>>>>> Stashed changes
     }
 }
---
+++ src/MyStackDC2.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+
+public class MyStackDC2 {
+
+    //Linked list to store Integer objects
+    LinkedList<Integer> MyList;
+    public MyStackDC2(){
+       MyList = new LinkedList<Integer>();
+    }
+
+    //get and removes the last element on the linked list
+    public int pop() {
+       return MyList.removeLast();
+    }
+    //add new element at the end
+    public void push(int element){
+        MyList.addLast(element);
+    }
+    //get last element
+    public int peek(){
+        return MyList.getFirst();
+    }
+
+    //check if the linked list is empty
+    //if linked list is empty return 0
+    public boolean isEmpty(){
+        return MyList.size() == 0;
+    }
+    //check if the linked list is full
+    //if it is full, list will return 4
+    public boolean isFull(int max){
+       return MyList.size() == max;
+    }
+
+}
